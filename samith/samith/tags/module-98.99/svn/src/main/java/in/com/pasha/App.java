d53178209edfba4a99d6d73ecb3d0a9cab054ed6
mohmmadh pasha <mohmmadh pasha>
2017-01-04 14:30:21 +0530

    added

---
+++ samith/samith/tags/module-98.99/svn/src/main/java/in/com/pasha/App.java
@@ -0,0 +1,13 @@
+package in.com.pasha;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
